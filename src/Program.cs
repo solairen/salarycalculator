18c237a2bbd77a81648e47110acf094a8203559c
solairen <michal.oleszek@outlook.com>
2026-02-28 21:41:49 +0100

    GH-111 [Task]: Add Pre-Commit Hooks, Markdownlint, and Secrets Detection

--- src/Program.cs
+++ src/Program.cs
@@ -1,24 +1,14 @@
 ﻿using System;
-using System.Threading;
 using CommandLine;
+using SalaryCalculator.Common;
 
 namespace SalaryCalculator
 {
     class Program
     {
-        public class Options
-        {
-            [Option('h', "hours", Required = true, HelpText = "Enter worked hours.")]
-            public decimal Hours { get; set; }
-
-            [Option('w', "hourly-wage", Required = true, HelpText = "Enter hourly wage.")]
-            public decimal HourlyWage { get; set; }
-        }
-
         static void Main(string[] args)
         {
-            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
-            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(options =>
+            Parser.Default.ParseArguments<OptionsHelper>(args).WithParsed<OptionsHelper>(options =>
             {
                 decimal workedHours = Math.Abs(options.Hours);
                 decimal hourlyWage = Math.Abs(options.HourlyWage);
